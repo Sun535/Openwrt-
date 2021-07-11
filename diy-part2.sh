4250794cd5ba9c673e0db5f336db16e0747e8f71
Sun535 <1677724535@qq.com>
2021-07-11 20:01:18 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -30,7 +30,7 @@ rm -rf openwrt-packages
 cd ../../
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
-TRUE
+
 
 
 :<<TRUE
